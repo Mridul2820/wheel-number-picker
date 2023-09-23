529497814d8e0b9d7ca35b7d6548044573ce07e9
Mridul Panda <immridulpanda2820@gmail.com>
2023-09-23 11:13:00 +0530

    Winning Number

--- src/components/WheelContent.js
+++ src/components/WheelContent.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import WheelComponent from "./WheelComponent";
 import Needle from "./icons/Needle";
 
 const WheelContent = () => {
+  const [numbers, setNumbers] = useState({
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+  });
+
   const segments = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const segColors = ["#0074BD", "#FAB515"];
-  const onFinished = (winner) => {
+
+  const onFinished = (winner, id) => {
     console.log(winner);
+    setNumbers((prevState) => {
+      return {
+        ...prevState,
+        [id]: winner,
+      };
+    });
   };
 
   return (
-    <div className="grid grid-cols-2 gap-10">
+    <div className="grid grid-cols-2 gap-10 items-center">
       <div className="p-2.5 relative min-h-screen">
         <div className="absolute-center">
           <WheelComponent
             segments={segments}
             segColors={segColors}
             winningSegment=""
-            onFinished={(winner) => onFinished(winner)}
+            onFinished={(winner) => onFinished(winner, 0)}
             primaryColor="black"
             primaryColoraround="#ffffffb4"
             contrastColor="white"
@@ -35,7 +50,7 @@ const WheelContent = () => {
             segments={segments}
             segColors={segColors}
             winningSegment=""
-            onFinished={(winner) => onFinished(winner)}
+            onFinished={(winner) => onFinished(winner, 1)}
             primaryColor="black"
             primaryColoraround="#ffffffb4"
             contrastColor="white"
@@ -53,7 +68,7 @@ const WheelContent = () => {
             segments={segments}
             segColors={segColors}
             winningSegment=""
-            onFinished={(winner) => onFinished(winner)}
+            onFinished={(winner) => onFinished(winner, 2)}
             primaryColor="black"
             primaryColoraround="#ffffffb4"
             contrastColor="white"
@@ -71,7 +86,7 @@ const WheelContent = () => {
             segments={segments}
             segColors={segColors}
             winningSegment=""
-            onFinished={(winner) => onFinished(winner)}
+            onFinished={(winner) => onFinished(winner, 3)}
             primaryColor="black"
             primaryColoraround="#ffffffb4"
             contrastColor="white"
@@ -89,7 +104,7 @@ const WheelContent = () => {
             segments={segments}
             segColors={segColors}
             winningSegment=""
-            onFinished={(winner) => onFinished(winner)}
+            onFinished={(winner) => onFinished(winner, 4)}
             primaryColor="black"
             primaryColoraround="#ffffffb4"
             contrastColor="white"
@@ -110,6 +125,23 @@ const WheelContent = () => {
           <span className="absolute-center text-white text-2xl font-bold mt-2">
             SPIN
           </span>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className={numbers[0] ? "numbers-active" : "numbers-inactive"}>
+          {numbers[0] ? numbers[0] : "5th Num"}
+        </div>
+        <div className={numbers[1] ? "numbers-active" : "numbers-inactive"}>
+          {numbers[1] ? numbers[1] : "4th Num"}
+        </div>
+        <div className={numbers[2] ? "numbers-active" : "numbers-inactive"}>
+          {numbers[2] ? numbers[2] : "3rd Num"}
+        </div>
+        <div className={numbers[3] ? "numbers-active" : "numbers-inactive"}>
+          {numbers[3] ? numbers[3] : "2nd Num"}
+        </div>
+        <div className={numbers[4] ? "numbers-active" : "numbers-inactive"}>
+          {numbers[4] ? numbers[4] : "1st Num"}
         </div>
       </div>
     </div>
